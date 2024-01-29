88277a55c23153457160113eb0c434e3b0fd7570
Louis D <coding@13bytes.de>
2024-01-29 21:08:52 +0100

    current state

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
 #include <CANCommunication.h>
+#include <math.h>
 
 #include "MPU9250.h"
 #include "game.h"
@@ -49,6 +50,24 @@ void setup() {
   LEDs.begin();
   LEDs.clear();
   LEDs.setBrightness(40);
+
+  Position_Vector apples = game.getApples();
+  Position_Vector snakePos = snake.getBody();
+
+  for (Position p : apples) {
+    matrix[x_y_to_matrix(p.x, p.y)] = 1;
+  }
+
+  // head other color
+  matrix[x_y_to_matrix(snakePos[0].x, snakePos[0].y)] = 2;
+  for (int i = 1; i < snakePos.size(); i++) {
+    matrix[x_y_to_matrix(snakePos[i].x, snakePos[i].y)] = 3;
+  }
+  for (int i = 0; i < NUM_PIXELS + 1; i++) {
+    int* color = get_color(matrix[i]);
+    LEDs.setPixelColor(i, LEDs.Color(color[0], color[1], color[2]));
+  }
+  LEDs.show();
 }
 
 void print_roll_pitch_yaw() {
@@ -76,34 +95,38 @@ void loop() {
       snake.moveRight();
     }
 
+    Position_Vector apples = game.getApples();
+    Position_Vector snakePos = snake.getBody();
+
+    for (Position p : apples) {
+      matrix[x_y_to_matrix(p.x, p.y)] = 1;
+    }
+
+    // head other color
+    matrix[x_y_to_matrix(snakePos[0].x, snakePos[0].y)] = 2;
+    for (int i = 1; i < snakePos.size(); i++) {
+      matrix[x_y_to_matrix(snakePos[i].x, snakePos[i].y)] = 3;
+    }
+    for (int i = 0; i < NUM_PIXELS + 1; i++) {
+      int* color = get_color(matrix[i]);
+      LEDs.setPixelColor(i, LEDs.Color(color[0], color[1], color[2]));
+    }
+    LEDs.show();
+
     packetWaiting = false;  // don't touch - reset and wait for new message
   }
 
-  Position_Vector apples = game.getApples();
-  Position_Vector snakePos = snake.getBody();
-
-  for (Position p : apples) {
-    matrix[x_y_to_matrix(p.x, p.y)] = 1;
-  }
-
-  // head other color
-  matrix[x_y_to_matrix(snakePos[0].x, snakePos[0].y)] = 2;
-  for (int i = 1; i < snakePos.size(); i++) {
-    matrix[x_y_to_matrix(snakePos[i].x, snakePos[i].y)] = 3;
-  }
-  for (int i = 1; i < NUM_PIXELS + 1; i++) {
-    int color[3] = {0, 255, 0};  // get_color(matrix[i]);
-    LEDs.setPixelColor(i, LEDs.Color(color[0], color[1], color[2]));
-  }
-  LEDs.show();
-
-  // Send data to CAN bus
+  // set data to CAN bus
   mpu.update();
   print_roll_pitch_yaw();
-  w_c1.x = mpu.getRoll();
-  w_c1.y = mpu.getPitch();
-  w_c1.z = mpu.getYaw();
-  w_c2.x = mpu.getAccX();
-  w_c2.y = mpu.getAccY();
-  w_c2.z = mpu.getAccZ();
+  w_c1.x = round(mpu.getRoll());
+  w_c1.y = round(mpu.getPitch());
+  w_c1.z = round(mpu.getYaw());
+  w_c2.x = round(mpu.getAccX());
+  w_c2.y = round(mpu.getAccY());
+  w_c2.z = round(mpu.getAccZ());
+  Serial.println(w_c1.x, 2);
+  Serial.println(w_c1.y, 2);
+  Serial.println(w_c1.z, 2);
+  delay(10);
 }

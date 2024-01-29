9e4bb1f8cbc09f340bf098b4977703f3b84ba997
Louis D <coding@13bytes.de>
2024-01-29 18:56:35 +0100

    working sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,49 @@
 #include <Arduino.h>
-
 #include <CANCommunication.h>
+
+#include "MPU9250.h"
+
+MPU9250 mpu;
 
 long currentMillis = 0;
 
 void setup() {
   Serial.begin(115200);
   //while (!Serial) delay(10);
+  Wire.begin();
+
   setupCANCommunication();
+  mpu.setup(0x68);
+  mpu.calibrateAccelGyro();
+  mpu.calibrateMag();
+}
+
+void print_roll_pitch_yaw() {
+  Serial.print("Yaw, Pitch, Roll: ");
+  Serial.print(mpu.getYaw(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getPitch(), 2);
+  Serial.print(", ");
+  Serial.println(mpu.getRoll(), 2);
 }
 
 void loop() {
   currentMillis = millis();
   loopCANCommunication(currentMillis);
 
-  // replace ths contents of the following if statement
-  // with your incoming coordinates update functionality
   if (packetWaiting) {
-    w_c1.x = r_c1.x + 1;
-    w_c1.y = r_c1.y + 1;
-    w_c1.z = r_c1.z + 1;
-    w_c2.x = r_c2.x + 1;
-    w_c2.y = r_c2.y + 1;
-    w_c2.z = r_c2.z + 1;
-    packetWaiting = false;
+    // ... new data arrived
+    packetWaiting = false;  // don't touch
   }
 
+  mpu.update();
+  print_roll_pitch_yaw();
+  w_c1.x = mpu.getRoll();
+  w_c1.y = mpu.getPitch();
+  w_c1.z = mpu.getYaw();
+  w_c2.x = mpu.getAccX();
+  w_c2.y = mpu.getAccY();
+  w_c2.z = mpu.getAccZ();
   // put your code to update the outgoing coordinates
   // here
 }
